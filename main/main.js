47bd4e53ee78488e6700981a4a30f4e531af74c7
Voi <1172673619@qq.com>
2017-06-24 22:25:44 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,55 @@
+"use strict";
+
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.number()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    var min,
+	max,
+	sum = 0;
+    for(var i=0,l=input.length;i<l;i++){
+	var value = input[i];
+        sum += value;
+	if(!i){
+		min  = value;
+		max  = value;
+		continue;
+	}
+	if(value<min){
+		min = value;
+	}
+	
+	if(value>max){
+		max = value;
+	}
+    }
+    this.min = min;
+    this.max = max;
+    this.sum = sum;
+    this.input = input;
+    //console.log(min,max,sum);
   }
 
   minimum() {
-    // Write your code here
+    return this.min;
+  }
+  
+  maximum(){
+    return this.max;
   }
 
-  // Write your code here
+  average(){
+    return (this.sum/this.input.length).toFixed(2);
+  }
+  
+  number(){
+    return this.input.length;
+  }
 }
